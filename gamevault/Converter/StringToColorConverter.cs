874a24ce44ddbcf8f581ed7a46ec18637039d6f1
Alper Alkan <alper.alkan2000@gmail.com>
2023-12-12 00:05:12 +0100

    fix: Bad Contrast String To Color Conversions

--- gamevault/Converter/StringToColorConverter.cs
+++ gamevault/Converter/StringToColorConverter.cs
@@ -9,14 +9,32 @@ namespace gamevault.Converter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            int hash = value.GetHashCode();
-
-            byte red = (byte)(hash & 0xFF);
-            byte green = (byte)((hash >> 8) & 0xFF);
-            byte blue = (byte)((hash >> 16) & 0xFF);
-
-            return new SolidColorBrush(Color.FromRgb(red, green, blue));
+            if (value is string stringValue)
+            {
+                int hashCode = stringValue.GetHashCode();
+                Color generatedColor = GenerateColor(hashCode);
+                SolidColorBrush brush = new SolidColorBrush(generatedColor);
+                brush.Freeze();
+                return brush;
+            }
+            return Brushes.Transparent;
         }
+
+        private Color GenerateColor(int numericValue)
+        {
+            byte red = (byte)((numericValue >> 16) & 0xFF);
+            byte green = (byte)((numericValue >> 8) & 0xFF);
+            byte blue = (byte)(numericValue & 0xFF);
+
+            // Darken the color by reducing each component by a fixed amount
+            const int darkeningAmount = 10;
+            red = (byte)Math.Max(0, red - darkeningAmount);
+            green = (byte)Math.Max(0, green - darkeningAmount);
+            blue = (byte)Math.Max(0, blue - darkeningAmount);
+
+            return Color.FromRgb(red, green, blue);
+        }
+
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
