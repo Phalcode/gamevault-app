678f5f7a487579557bd3fba943f527fb6968f95f
Philip <pipo.scha@web.de>
2024-02-12 11:41:14 +0100

    Bug fix: re-download icon not appearing on library

---
+++ gamevault/Converter/IsGameDownloadedConverter.cs
@@ -0,0 +1,29 @@
+﻿using gamevault.Models;
+using gamevault.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace gamevault.Converter
+{
+    internal class IsGameDownloadedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.WriteLine("IsDownloaded");
+            if (value == null)
+                return false;
+            return DownloadsViewModel.Instance.DownloadedGames.Where(gameUC => gameUC.GetGameId() == (int)value).Count() > 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return false;
+        }
+    }
+}
